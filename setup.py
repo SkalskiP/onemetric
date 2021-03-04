5a81c82f9a479cfd1ac5dee56fee1af6107a382d
SkalskiP <piotr.skalski92@gmail.com>
2021-03-04 22:55:08 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="ml-metrics",
+    version='0.0.1',
+    description="Metrics To Evaluate Machine Learning Algorithms in Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SkalskiP/ml-metrics",
+    author="Piotr Skalski",
+    author_email="piotr.skalski92@gmail.com",
+    license='BSD',
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
